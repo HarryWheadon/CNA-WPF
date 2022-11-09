aa88e9db7b853a59f5b0c99f81fdbebadd2079e3
Harry Wheadon <91532361+HarryWheadon@users.noreply.github.com>
2022-11-09 10:56:19 +0000

    Created a client & server for WPF project

---
+++ CNA WPF/Client.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNA_WPF
+{
+    internal class Client
+    {
+        private NetworkStream m_stream;
+        private StreamWriter m_writer;
+        private StreamReader m_reader;
+        private TcpClient m_tcpClient;
+
+        public Client()
+        {
+            m_tcpClient = new TcpClient(); 
+        }
+
+        public bool Connect(string ipAddress, int port)
+        {
+            try
+            {
+                m_tcpClient.Connect(ipAddress, port);
+                m_stream = m_tcpClient.GetStream();
+                m_writer = new StreamWriter(m_stream, Encoding.UTF8);
+                m_reader = new StreamReader(m_stream, Encoding.UTF8);
+                return true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                return false;
+            }
+
+        }
+
+        public void Run()
+        {
+            string userInput;
+            ProcessServerResponse();
+            while((userInput = Console.ReadLine()) != null)
+            {
+                m_writer.WriteLine(userInput);
+                m_writer.Flush();
+
+                ProcessServerResponse();
+
+                if(userInput == ("end"))
+                {
+                    break;
+                }
+
+                m_tcpClient.Close();
+            }
+        }
+
+        private void ProcessServerResponse()
+        {
+            Console.WriteLine("Server says: " + m_reader.ReadLine());
+        }
+    }
+}
---
+++ ServerProj/Server.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace ServerProj
+{
+    internal class Server
+    {
+        private TcpListener m_TcpListener;
+
+        public Server(string ipAddress, int port)
+        {
+            IPAddress ip = IPAddress.Parse(ipAddress);
+            m_TcpListener = new TcpListener(ip, port);
+        }
+
+
+        public void Start()
+        {
+            m_TcpListener.Start();
+            Console.WriteLine("Listening...");
+
+            Socket socket = m_TcpListener.AcceptSocket();
+            Console.WriteLine("Connection Made");
+            ClientMethod(socket);
+        }
+        public void Stop()
+        {
+            m_TcpListener.Stop();
+        }
+
+        private void ClientMethod(Socket socket)
+        {
+            string receivedMessage;
+
+            NetworkStream stream = new NetworkStream(socket, true);
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            StreamWriter writer = new StreamWriter(stream, Encoding.UTF8);
+
+            writer.WriteLine("You have connected to the server - send 0 for valid options");
+            writer.Flush();
+
+            while((receivedMessage = reader.ReadLine()) != null)
+            {
+                writer.WriteLine(GetReturnMessage(receivedMessage));
+                writer.Flush();
+
+                if(receivedMessage == "end")
+                {
+                    break;
+                }
+            }
+            socket.Close();
+        }
+
+        private string GetReturnMessage(string code) 
+        {
+            switch(code)
+            {
+                case "0":
+                    return ("you have chosen 0");
+                case "1":
+                    return ("you have chosen 1");
+                case "2":
+                    return ("you have chosen 2");
+                case "3":
+                    return ("you have chosen 3");
+                case "4":
+                    return ("you have chosen 4");
+                case "5":
+                    return ("you have chosen 5");
+                case "6":
+                    return ("you have chosen 6");
+                case "7":
+                    return ("you have chosen 7");
+                case "8":
+                    return ("you have chosen 8");
+                case "9":
+                    return ("you have chosen 9");
+                case "10":
+                    return ("you have chosen 10");
+            }
+            return "hello";
+        }
+    }
+}
